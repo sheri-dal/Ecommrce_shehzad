97a17be037313ca4134a1d235ec5824b292a6f5a
sheri-dal <shaharyarhussain0@gmail.com>
2022-03-05 13:12:21 +0500

    Testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shehzad_ecoomrce/main.dart';
+import 'package:shehzad_ecoomrce/View/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
